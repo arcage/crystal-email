851056769609c018736b728a2c54c0c54c5c6c32
ʕ·ᴥ·ʔAKJ <arcage@denchu.org>
2019-12-17 12:00:43 +0900

    v0.4.4
    - `EMail::Client#send` returns sending result as `Bool`.
    - Add minmal specs for sending emails.
    - Add CI setting.

--- spec/email_spec.cr
+++ spec/email_spec.cr
@@ -1,4 +1,3 @@
 require "./spec_helper"
 
-describe EMail do
-end
+require "./email/*"
--- src/email.cr
+++ src/email.cr
@@ -8,7 +8,7 @@ require "uri"
 require "./email/*"
 
 module EMail
-  VERSION           = "0.4.3"
+  VERSION           = "0.4.4"
   DEFAULT_SMTP_PORT = 25
 
   # :nodoc:
